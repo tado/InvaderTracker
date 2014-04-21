1dc026963fa4a9c4398364113e1c0fec6ca07fea
moxuse <moxuse@gmail.com>
2014-04-21 13:32:24 +0900

    データセット後にグラフを再秒が

--- js/telemetry/view/GraphView.js
+++ js/telemetry/view/GraphView.js
@@ -8,36 +8,75 @@ var GraphView = Backbone.View.extend({
     graphSvg: undefined,
     tagName: 'svg',
   },
-  width:  800,
+  width:  900,
   height: 250,
+  graphPath: undefined,
+  line: undefined,
   initialize: function() {
     this.graphSvg = d3.select("#graphView").append("svg:svg").attr("width", '1000px').attr("height", '980px');
+  },
+  scaleY: function(arr) {
+    var scale = d3.scale.linear()
+        .domain([d3.min(arr), d3.max(arr)])
+        .range([0, this.height - 5]);
+    return scale;
+  },
+  scaleX: function(arr) {
+    var scale = d3.scale.linear()
+        .domain([0, arr.length])
+        .range([0, this.width]);
+    return scale;
   },
   render: function() {
     var self = this;
     if (0 < this.collection.length && this.collection != null) {
-
       var dataArray = self.collection.pluck('alt');
 
-      var ymin = d3.min(dataArray);
-      var ymax = d3.max(dataArray);
-      
-      var scaleX = d3.scale.linear()
-        .domain([0, dataArray.length])
-        .range([0, self.width]);
-      var scaleY = d3.scale.linear()
-        .domain([ymin, ymax])
-        .range([0, self.height]);
+      var scaleX = self.scaleX(dataArray);
+      var scaleY = self.scaleY(dataArray);
 
-      var line = d3.svg.line()
+      self.line = d3.svg.line()
         .x(function(d,i) { return scaleX(i) })
-        .y(function(d,i) { return scaleY(d);})
-        .interpolate("linear");//点の繋ぎ方の指定
-      self.graphSvg.append("path")
-        .attr("d", line(dataArray))
+        .y(function(d,i) { return self.height - scaleY(d);})
+        .interpolate("monotone")//点の繋ぎ方の指定
+
+      self.graphPath = self.graphSvg.append("path")
+        .attr("d", self.line(dataArray))
         .attr("stroke", "white")
-        .attr("stroke-width", 2)
+        .attr("stroke-width", 1)
         .attr("fill", "none");
+    }
+
+    $("#graphView").on("click", function() {
+      var data = ["bv","bv","gx","gy","gz","mx","my","mz","scpx","scpy","scpz"];
+      var index = Math.floor((Math.random()*10));
+      console.log("clicked",index);
+      self.transformDataTo(data[index]);
+    });
+  },
+  transformDataTo: function(type) {
+    var self = this;
+    
+    var dataArray = self.collection.pluck(type);
+
+    var scaleX = self.scaleX(dataArray);
+    var scaleY = self.scaleY(dataArray);
+
+    var newline = d3.svg.line()
+        .x(function(d,i) { return scaleX(i) })
+        .y(function(d,i) { return self.height - scaleY(d);})
+        .interpolate("monotone")//点の繋ぎ方の指定
+
+    self.graphPath
+      .transition()
+      .duration(300)
+      .attr("d", newline(dataArray))
+  },
+  isNumber: function(x) { 
+    if (typeof(x) != 'number' && typeof(x) != 'string') {
+        return false;
+    } else {
+        return (x == parseFloat(x) && isFinite(x));
     }
   }
 })
