2c4eb5c3a0298abb03ec6818580840327507029b
Atsushi Tadokoro <tadokoro@gmail.com>
2014-07-14 08:20:09 +0900

    Revert 6438038..f040249
    This rolls back to commit 64380384c57a4c07d613cf08ff418a431757b240.

--- js/main.js
+++ js/main.js
@@ -21,8 +21,6 @@ var loadedTle = new Array(100);
 var tmpTle;
 var orbitLine;
 var pointMesh = new Array(100);
-var projector;
-var overlayView;
 
 loadData();
 
@@ -111,8 +109,6 @@ function init() {
     camera.position.z = -18000;
     scene = new THREE.Scene();
     group = new THREE.Object3D();
-    projector = new THREE.Projector(window.innerWidth, window.innerHeight);
-    overlayView = new OverlayView(window.innerWidth, window.innerHeight);
     scene.add( group );
 
     /* earth */
@@ -162,8 +158,6 @@ function init() {
 
     /* draw orbit */
     drawOrbit();
-
-    drawGraphView();
 }
 
 function onWindowResize() {
@@ -172,7 +166,6 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
-    overlayView.setSize( window.innerWidth, window.innerHeight );
 }
 
 function animate() {
@@ -208,12 +201,6 @@ function animate() {
         + tle.second_line
         + "</p>");
 
-    /* set projected position to CSS2DRenderer */
-    var target = satellite_mesh;
-    var projectedPosition = getProjection(target);
-    overlayView.setBillboardPosition(projectedPosition);
-    overlayView.update();
-
     /* animate */
     requestAnimationFrame(animate);
     render();
@@ -236,27 +223,3 @@ function addPoint(lat, lng, alt) {
 
     return pos;
 }
-
-function getProjection(target) {
-  var vector = projector.projectVector( new THREE.Vector3(target.position.x, target.position.y, target.position.z), camera );
-  vector.x = vector.x * window.innerWidth * 0.5;
-  vector.y = -( vector.y * window.innerHeight * 0.5 );
-  return vector;
-}
-
-function drawGraphView() {
-    var sensors = new Sensors();
-    var graphView = new GraphView({
-        collection: sensors
-    });
-    sensors.fetch({
-        dataType : 'jsonp',
-        success: function(collection, res, options) {
-            graphView.render();
-        },
-        error: function(e) {
-            console.log("error-- ", e);
-        }
-    })
-}
-
